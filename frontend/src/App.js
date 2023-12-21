6c9d3da82853b0c5ea9536b904611ab8ed669b84
SaiSharan2005 <duginisaisharan@gmail.com>
2023-12-22 05:06:59 +0530

    adding blockchain

---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+import React,{useState,useEffect} from "react";
+import { Authentication, Login } from "./authentication.js";
+import Credits from "./registration.js";
+import Otp from "./otp.js";
+import Microid from "./microid.js";
+import AddConsumer from "./components/AddConsumer.js";
+import { ethers } from "ethers";
+// import contractABI from "./blockChain/Microgrid.json";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import {connectToMetaMask} from "./hooks/metaMaskService.js"
+
+export default function App() {
+  const [temp, setTemp] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { sendDataContract, getDataContract } = await connectToMetaMask();
+        // Use sendDataContract and getDataContract here or set them to state variables
+        console.log(sendDataContract, getDataContract);
+
+        // Set temp state with received data
+        setTemp(getDataContract); // Replace with the data you want to display
+      } catch (error) {
+        // Handle errors
+        console.error("Error:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const myrouter = createBrowserRouter([
+    { path: "/", element: <Authentication /> },
+    { path: "/registration", element: <Credits /> },
+    { path: "/otp", element: <Otp /> },
+    { path: "/sucess", element: <Microid /> },
+    { path: "/login", element: <Login /> },
+    { path: "/addConsumer", element: <AddConsumer  contract = {temp}/> },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={myrouter} />
+    </div>
+  );
+}
